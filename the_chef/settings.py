85d2fe07c4bdbf87b61418b802a30be1385903b2
Tiago Lourenco <tiagolourenco313@gmail.com>
2024-12-25 23:31:36 +0000

    dj_database_config

--- the_chef/settings.py
+++ the_chef/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 from django.core.management.utils import get_random_secret_key
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -72,24 +73,12 @@ WSGI_APPLICATION = "the_chef.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default='postgresql://postgres:rootuser@localhost:5432/the_chef',
-#         conn_max_age=600,  # Keeps database connections alive for 600 seconds.
-#         ssl_require=False  # Set to True if using SSL in production.
-#     )
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB"),  # Database name
-        "USER": os.getenv("POSTGRES_USER"),  # Database user
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),  # Database password
-        "HOST": os.getenv("POSTGRES_HOST"),  # Database host
-        "PORT": os.getenv("POSTGRES_PORT"),  # Database port
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL", "postgresql://postgres:rootuser@localhost:5432/the_chef")
+    )
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
