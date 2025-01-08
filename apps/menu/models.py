70fd5e3e87e3639d4a300bd087d0032d63f2467f
Tiago Lourenco <lourenco123tiago@gmail.com>
2025-01-08 15:23:28 +0000

    Update project

--- apps/menu/models.py
+++ apps/menu/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="dishes")
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
